c8d38729e518f8cc4288cb70547991be68304847
Justin <justin.bellavance@gmail.com>
2024-12-31 16:13:33 -0500

    big update

---
+++ components/Footer/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Socials from "../Socials";
+
+const Footer = ({}) => {
+  return (
+    <>
+      <div className="mt-10">
+        <Socials />
+      </div>
+    </>
+  );
+};
+
+export default Footer;
