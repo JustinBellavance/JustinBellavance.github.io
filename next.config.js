4827d3997cfa965a8e5daa5c96ca20da8fcc168e
Justin <justin.bellavance@gmail.com>
2024-12-31 16:50:57 -0500

    vercel instead of github pages

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  output: 'export',
+  basePath: '/JustinBellavance.github.io',
+  assetPrefix: '/JustinBellavance.github.io',
+};
 
 module.exports = nextConfig
