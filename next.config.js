7a5aa0efca7dd7a543dbd5114d8698692888063f
Justin <justin.bellavance@gmail.com>
2025-01-01 11:26:22 -0500

    css bug fixes?

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'export',
-  basePath: '/JustinBellavance.github.io',
-  assetPrefix: '/JustinBellavance.github.io',
 };
 
 module.exports = nextConfig
